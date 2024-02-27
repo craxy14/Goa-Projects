962a9fedd31fe1744856e1415c674a05d1e260ab
craxy14 <xxmarselxx5@gmail.com>
2024-02-27 20:57:57 +0400

    iforgot commit

---
+++ Day 49/classwork/script.js
@@ -0,0 +1,35 @@
+// me = {
+//     name: "vano",
+//     surname: "motiashvili",
+//     age: 16,
+
+//     changeAge: function(){
+//         console.log("Old age =" , me.age)
+//         me.age = prompt("Enter new age: ")
+//         console.log("New age =" , me.age)
+//     },
+
+//     hobbies: {
+//         wrestling: true,
+//         gym: true,
+//         eating: true
+//     }
+// }
+
+// me.changeAge()
+// console.log(me)
+
+
+// //Creating primitive variables: 
+
+// myName = "Vano"
+// age = 16
+// height = 186.5
+// isFat = true
+
+
+// myName2 = myName
+// console.log(myName2)
+
+
+// //(მესამე დავალებას ვერმივხვდი ლუკა ესიგულისხმე თუ არა არვიცი...)
